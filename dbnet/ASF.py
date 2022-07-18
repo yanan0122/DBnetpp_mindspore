feaa91704461e1b2f6966c8a6a31725d5af77eac
SakiRinn <wlh1433537034@gmail.com>
2022-07-18 17:42:56 +0800

    ASF done.

--- dbnet/ASF.py
+++ dbnet/ASF.py
@@ -1,9 +1,9 @@
-from cv2 import split
-import mindspore
-from mindspore import Tensor
 import mindspore.nn as nn
 import mindspore.ops as ops
 
+from mindspore import context
+from numpy import expand_dims
+context.set_context(device_id=5, mode=context.GRAPH_MODE)
 
 class SpatialAttention(nn.Cell):
     def __init__(self, inner_channels, N):
@@ -23,12 +23,12 @@ class SpatialAttention(nn.Cell):
 
         self.reduce_mean = ops.ReduceMean(True)
         self.expand_dims = ops.ExpandDims()
-        self.split = ops.Split(0, N)
+        self.split = ops.Split(1, N)
 
     def construct(self, x):
         x1 = self.channel_wise(x)
 
-        x2 = self.reduce_mean(x1)
+        x2 = self.reduce_mean(x1, 1)
         x2 = self.spatial_wise(x2)
         x2 = self.relu(x2)
         x2 = self.spatial_wise(x2)
@@ -38,8 +38,7 @@ class SpatialAttention(nn.Cell):
         y = self.attention_wise(y)
         y = self.sigmoid(y)
 
-        Y = self.expand_dims(y, 1)
-        return self.split(Y)
+        return self.split(y)
 
 
 class ASF(nn.Cell):
@@ -54,13 +53,32 @@ class ASF(nn.Cell):
         self.spatial_attention = SpatialAttention(inner_channels, self.N)
 
         self.concat = ops.Concat()
+        self.expand_dims = ops.ExpandDims()
 
-    def construct(self, X):
-        if len(X) != self.N:
+    def construct(self, x):
+        if len(x) != self.N:
             exit(1)
+        X = self.expand_dims(self.concat(x), 0)
 
-        S = self.conv(self.concat(X))
+        S = self.conv(X)
         A = self.spatial_attention(S)
-        F = self.concat([ops.matmul(A[i], X[i]) for i in self.N])
 
-        return F
+        F = self.concat([(A[i] * x[i]).squeeze() for i in range(self.N)])
+        return self.expand_dims(F, 0)
+
+
+if __name__ == '__main__':
+    std = ops.StandardNormal()
+    split = ops.Split(output_num=4)
+    concat = ops.Concat()
+
+    X = std((4, 64, 50, 100))
+    X = split(X)
+    X = list(X)
+    for i in range(len(X)):
+        X[i] = X[i].squeeze()
+    y = concat(X)
+
+    asf = ASF()
+    result = asf(X)
+    print(result.shape)
